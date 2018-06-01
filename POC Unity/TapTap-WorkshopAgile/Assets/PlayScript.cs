90d77430387e240d4431318877747dcca742c120
D3mD3m <adem.issad@epsi.fr>
2018-06-01 15:06:08 +0200

    UnityPOC
    Proof Of Concept on Unity software
    Now git available this 2 task are in :
    Game launch
    Monster display

---
+++ POC Unity/TapTap-WorkshopAgile/Assets/PlayScript.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayScript : MonoBehaviour {
+
+
+    //Make sure to attach these Buttons in the Inspector
+    public Button m_YourButton, m_YourSecondButton;
+
+    // Use this for initialization
+    void Start()
+    {
+        Button btn = m_YourButton.GetComponent<Button>();
+        Button btn2 = m_YourSecondButton.GetComponent<Button>();
+        //Calls the TaskOnClick method when you click the Button
+        btn.onClick.AddListener(TaskOnClick);
+
+        m_YourSecondButton.onClick.AddListener(delegate { TaskWithParameters("Hello"); });
+    }
+
+    private void TaskOnClick()
+    {
+        Debug.Log("You have clicked the button!");
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+}
