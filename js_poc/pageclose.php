4c97b3ef74f6f7dcee8fe027d13b7e93d63a4691
Xavier <zeloftmail@yahoo.fr>
2018-06-11 10:18:00 +0200

    exit button integration

---
+++ js_poc/pageclose.php
@@ -0,0 +1,9 @@
+<head>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+</head>
+
+
+
+<p class="text-danger">Merci, l'équipe continue le développement</p>
+
+<h2> adresse btc: ijeij585Nihihi4IHHkniiik7655juhuhçNJUJJ </h2>
